365c1a46f600a6a9c3a4dca026c0630beebb8a14
Bontusalih90 <bontusalih1@gmail.com>
2024-07-03 01:41:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ if (closebtn) {
         navbar.classList.remove('active');
     })
 }
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signUp');
+
+signUpButton.addEventListener('click', function(){
+    signInForm.style.display="none";
+    signUpForm.style.display="block";
+})
+signInButton.addEventListener('click', function(){
+    signUpForm.style.display="none";
+    signInForm.style.display="block";
+})
